3ae59816ed310c13cba3e2d3e267e801ba70c281
FranckHeCai <senlouboi.egame@gmail.com>
2025-08-11 20:28:29 +0200

    logical operators

--- 02_flow_control/01_if.py
+++ 02_flow_control/01_if.py
@@ -1,5 +1,8 @@
+import os
+os.system("clear")
+
 print("Simple conditional statements")
-edad = 18
+edad = 99
 if edad >= 18:
     print("Eres mayor de edad")
 else:
@@ -19,3 +22,23 @@ elif nota >= 5:
     print("Aprobado")
 else:
     print("Suspendido")
+
+print("Multiple conditions with logical operators")
+tiene_carnet = True
+
+if edad >= 18 and edad <=80 and tiene_carnet:
+    print("Puedes conducir")
+else:
+    print("No puedes conducir")
+
+if edad >= 18 or tiene_carnet:
+    print("Puedes conducir")
+else:
+    print("Sobarna al policia")
+
+fin_de_semana = False
+
+if not fin_de_semana:
+    print("A trabajar")
+else:
+    print("A descansar")
