1b6b28818b36fbd293118b647d3a7b2043fc0e50
FranckHeCai <senlouboi.egame@gmail.com>
2025-09-08 19:52:38 +0200

    playground

--- Bootcamp/playground.py
+++ Bootcamp/playground.py
@@ -1,3 +1,14 @@
-#sec = int(input("segundos: "))
-print(4000/3600)
+max = 0
 
+num1 = int(input())
+max = num1
+
+num2 = int(input())
+if num2 > max:
+  max = num2
+
+num3 = int(input())
+if num3 > max:
+  max = num3
+
+print(max)
