a0106a1a4a0f1e0ab89dd87613135882a210e29d
FranckHeCai <senlouboi.egame@gmail.com>
2025-09-09 18:19:48 +0200

    pyramid of stars

--- Bootcamp/playground.py
+++ Bootcamp/playground.py
@@ -1,14 +1,18 @@
-max = 0
+stars = int(input())
+size = (stars * 2)
+contador = 1
 
-num1 = int(input())
-max = num1
+while contador < stars + 1:
+    print("*" * contador)
+    contador += 1
 
-num2 = int(input())
-if num2 > max:
-  max = num2
+contador = stars - 1
 
-num3 = int(input())
-if num3 > max:
-  max = num3
+# while contador > 0:
+#     prints = contador
 
-print(max)
+#     while prints < contador + 1:
+#         print("*" * prints)
+#         prints-= 1
+
+#     contador -= 1
