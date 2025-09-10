413e50ce35df9709c4cb40cf537dc95935e107e2
FranckHeCai <senlouboi.egame@gmail.com>
2025-09-10 19:51:29 +0200

    functions

--- Bootcamp/playground.py
+++ Bootcamp/playground.py
@@ -1,10 +1,19 @@
-lista = []
-lista_ordenada = []
+def comprobar_longitud(str):
+    return len(str) >= 7
 
-for _ in range(0,5):
-  num = int(input(f"Numero {_ + 1}: "))
-  lista.append(num)
-  
+def comprobar_primero(str):
+    return str[0].isupper()
 
-print(lista)
+def comprobar_ultimo(str):
+    return str[len(str)-1].isnumeric()
 
+password = input()
+
+if not comprobar_longitud(password):
+    print("Incorrecta. Debe comenzar tener al menos 7 caracteres")
+elif not comprobar_primero(password):
+    print("Incorrecta. Debe comenzar con una vocal mayúscula")
+elif not comprobar_ultimo(password):
+    print("Incorrecta. Debe acabar con un numero")
+else:
+    print(f"Correcta. La contraseña es: {password}")
